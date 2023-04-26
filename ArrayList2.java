d93e2e5345301dfff9ef344bdb41b66436a2aa0f
Amirhan-Botagariev <129430641+Amirhan-Botagariev@users.noreply.github.com>
2023-04-26 09:34:44 +0600

    Add files via upload

---
+++ ArrayList2.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class ArrayList2 {
+    private int capacity = 50;
+    private int size = 0;
+    private int[] ArrayList = new int[capacity];
+
+    public void addAll(int index, ArrayList<Integer> toInsert){
+        int x = 0, y = 0;
+        int[] ArrayList2 = new int[capacity];
+        for (int i = 0; i < index; i++){
+            ArrayList2[i] = ArrayList[i];
+            x++;
+        }
+        for (int i = 0; i < toInsert.size(); i++){
+            ArrayList2[x] = toInsert.get(y);
+            x++;
+            y++;
+            size++;
+        }
+        y = index;
+        while (ArrayList[y] != 0){
+            ArrayList2[x] = ArrayList[y];
+            y++;
+            x++;
+        }
+        ArrayList = ArrayList2;
+        reduceRedundant();
+    }
+
+    public void add(int item) {
+        if (size == capacity){
+            increaseSizeOfArrayList();
+        }
+        ArrayList[size++] = item;
+    }
+    private void increaseSizeOfArrayList(){
+        capacity = 2 * capacity;
+        int[] ArrayList2 = new int[capacity];
+        for(int i = 0; i < size; i++){
+            ArrayList2[i] = ArrayList[i];
+        }
+        ArrayList = ArrayList2;
+    }
+
+    private void reduceRedundant(){
+        int k = size;
+        capacity = k;
+        int[] ArrayList2 = new int[capacity];
+        for (int i = 0; i < k; i++){
+            ArrayList2[i] = ArrayList[i];
+        }
+        ArrayList = ArrayList2;
+    }
+
+
+
+
+}
